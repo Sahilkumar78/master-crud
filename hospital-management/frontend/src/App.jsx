8bcb548056b7f4a7a0fe94968fee11c490ec5d14
SahilKumar78 <sahilabcd661@gmail.com>
2026-08-27 12:46:17 +0530

    frontend setup done

---
+++ hospital-management/frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+
+
+function App() {
+
+
+  return (
+     <>
+     <h1 className='bg-red-400' >Vedanta hospital management</h1>
+     </>
+  )
+}
+
+export default App
