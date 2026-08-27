bf4892c3271e34e8b4e86d7f18967499fa19aed6
SahilKumar78 <sahilabcd661@gmail.com>
2026-08-27 12:37:30 +0530

    backend done

--- hospital-management/backend/controllers/Doctor.controller.js
+++ hospital-management/backend/controllers/Doctor.controller.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-
+import { Doctor } from "../models/Doctor.model.js";
 
 // getDoctors
 
